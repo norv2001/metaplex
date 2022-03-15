9216b8993e7e0b3ff6fc6077a764e252240d0381
norv2001 <norv2001@gmail.com>
2022-03-14 18:51:25 -1000

    Customizing vanilla Storefront with new logo etc

--- js/packages/web/src/views/home/components/SalesList/index.tsx
+++ js/packages/web/src/views/home/components/SalesList/index.tsx
@@ -43,9 +43,9 @@ export const SalesListView = (props: { collectionMintFilter?: string }) => {
     <>
       {!props.collectionMintFilter && (
         <Banner
-          src="/main-banner.svg"
-          headingText="The amazing world of Metaplex."
-          subHeadingText="Buy exclusive Metaplex NFTs."
+          src="/theKeep-sans-white.svg"
+          headingText="The amazing world of NFTs."
+          subHeadingText="Buy 1/1s, editions, and PFPs."
           actionComponent={<HowToBuyModal buttonClassName="secondary-btn" />}
           useBannerBg
         />
